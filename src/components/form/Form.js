006c6e3dbd237e513f459dd0481000cc89baa63b
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2022-11-20 14:21:50 -0300

    Form structure completed, use of useState

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,37 +1,48 @@
 import React, { useState } from "react"
-import { Firestore } from "@firebase/firestore";
+// import { Firestore } from "@firebase/firestore";
 
 import './form.css'
 
-function Form () {
+const Form = () => {
 
-const AddItem = ({ firestore }) => {
-    const [name, setName] = useState('');
-};
+  const [details, setDetails] = useState({
+    name: "",
+    email: "",
+    address: "",
+    peopleNumber: "",
+    phone: "",
+    reservationTime: "",
+    comments: "",
+  })
 
-const addItem = name => {
-    firestore.collection('items').add({ name });
-};
-const handleChange = event => {
-    setName(event.target.value);
- };
-const handleSubmit = event => {
-    event.preventDefault();
-    addItem(name);
- };
+  const handleChange = (e) => {
+    const {name, value} = e.target
+  
+    setDetails((prev) => {
+      return { ...prev, [name]:value}
+    })
+  }
 
-return (
-    <main>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Add Item:
-          <input value={name} type="text" id="name" onChange={handleChange} />
-        </label>
-        <button onClick={handleSubmit}>Add item</button>
-      </form>
-    </main>
-);
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(details);
+  }
 
+
+  return(
+    <form onSubmit={handleSubmit}>
+      <h4>Name:</h4> <input type="text" name="name" id="name" onChange={handleChange}/>
+      <h4>Email:</h4> <input type="email" name="email" id="email" onChange={handleChange}/>
+      <h4>Address:</h4> <input type="text" name="address" id="address" onChange={handleChange}/>
+      <h4>Number of people (between 1 and 5):</h4> <input type="range" min="1" max="5" name="peopleNumber" id="peopleNumber" onChange={handleChange}/>
+      <h4>Phone Number:</h4> <input type="tel" name="phone" id="phone" onChange={handleChange}/>
+      <h4>Time and Date of reservation:</h4> <input type="datetime-local" name="reservationTime" id="reservationTime" onChange={handleChange}/>
+      <h4>Additional Comments:</h4> <textarea type="text" name="comments" id="comments" onChange={handleChange}/>
+      <button type="submit">Submit Reservation</button>
+    </form>
+  )
 }
+
+
 
 export default Form;
