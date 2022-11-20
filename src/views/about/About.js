8b04aecfeebc475c032e14be8aede985f629a51c
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2022-11-20 14:51:34 -0300

    Components design draft

--- src/views/about/About.js
+++ src/views/about/About.js
@@ -3,7 +3,10 @@ import React from "react";
 function Book () {
     return (
         <div>
-            <h1>Book</h1>
+            <h1>About</h1>
+            <h2>
+            Lorem ipsum dolor sit amet. Est accusantium ipsa aut odit quisquam sed consequuntur mollitia et quos ratione qui repellat odio et sequi voluptates? Sed enim error et itaque sequi At voluptatem corporis est iusto asperiores in optio reiciendis sit saepe corrupti. Et voluptatem cupiditate est voluptatem officiis ut tempore repellat non quae molestiae et odio sunt aut molestiae eveniet aut facere repellat. Pellat aut sint quia et nulla facere eum expedita voluptatem aut facere nesciunt qui esse alias. Qui nulla animi sit vitae reprehenderit qui adipisci quibusdam rem quibusdam voluptatibus. Quo deserunt sunt qui officiis velit qui consequatur minima hic consequuntur doloremque. Ab aliquam odit eos ratione inventore sed nesciunt explicabo et dolor labore et praesentium voluptas in officiis enim aut magni quas. Est veritatis vitae est quidem voluptates eos galisum odio ex illum expedita aut veniam voluptatibus vel vitae iure. Eum odit dignissimos qui laborum cumque At excepturi dolorem quo fugiat impedit sed labore repellat ad dolorem fuga.
+            </h2>
         </div>
     )
 }
