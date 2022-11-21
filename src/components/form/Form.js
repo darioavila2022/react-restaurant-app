5b21d2a204ef1d5a620a78a73fb349ae187daa26
darioavila2022 <daavila.dwfsc2@bootcampudd.cl>
2022-11-21 15:07:50 -0300

    Change type of input from "Range" to "Select" for "peopleNumber"

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,6 +3,7 @@ import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../config/firestore";
 
 import './form.css'
+import modalImage from '../../images/modalImage.jpg'
 
 const Form = () => {
   const modal = document.querySelector(".modal-wrap")
@@ -27,15 +28,15 @@ const Form = () => {
       console.log("Document written with ID: ", docRef.id);
       modal.classList.toggle('display-none')
       close.addEventListener("click", () => {
-      modal.classList.toggle('display-none')
+        modal.classList.toggle('display-none')
       })
     } catch (e) {
-      console.error("Error in adding document ", e)
+      console.error("Error in adding document", e)
     } finally {
       document.getElementById('contactForm').reset()
     }
   }
-  
+
 
   function handleChange(event) {
     const { target } = event
@@ -48,20 +49,28 @@ const Form = () => {
     <div>
       <div className="modal-wrap display-none">
         <div className="modal">
-          <img src="https://i0.wp.com/t-artmagazine.com/wp-content/uploads/2021/04/vector-creator.png?fit=800%2C534&ssl=1" alt="" />
+          <img src={modalImage} alt="" />
           <p>Thank you for your reservation!</p>
           <p>See you at the restaurant</p>
           <button className="btn modal-close">Close</button>
         </div>
       </div>
       <form id="contactForm" className="contact-form" onSubmit={handleSubmit}>
-        <h4>Name:</h4> <input type="text" className="name" name="name" id="name" onChange={handleChange} />
-        <h4>Email:</h4> <input type="email" className="email" name="email" id="email" onChange={handleChange} />
-        <h4>Address:</h4> <input type="text" className="address" name="address" id="address" onChange={handleChange} />
-        <h4>Number of people (between 1 and 5):</h4> <input className="peopleNumber" type="range" min="1" max="5" name="peopleNumber" id="peopleNumber" onChange={handleChange} />
-        <h4>Phone Number:</h4> <input type="tel" className="phone" name="phone" id="phone" onChange={handleChange} />
-        <h4>Time and Date of reservation:</h4> <input type="datetime-local" className="reservationTime" name="reservationTime" id="reservationTime" onChange={handleChange} />
-        <h4>Additional Comments:</h4> <textarea type="text" className="comments" name="comments" id="comments" onChange={handleChange} />
+        <h4>Name:</h4> <input type="text" name="name" onChange={handleChange} />
+        <h4>Email:</h4> <input type="email" name="email" onChange={handleChange} />
+        <h4>Address:</h4> <input type="text" name="address" onChange={handleChange} />
+        {/* <h4>Number of people:</h4> <input  type="range" min="1" max="5" name="peopleNumber" onChange={handleChange} /> */}
+        <h4>Table for:</h4>
+        <select name="peopleNumber" onChange={handleChange}>
+          <option value="1">1 Person</option>
+          <option value="2">2 People</option>
+          <option value="3">3 People</option>
+          <option value="4">4 People</option>
+        </select>
+
+        <h4>Phone Number:</h4> <input type="tel" name="phone" onChange={handleChange} />
+        <h4>Time and Date of reservation:</h4> <input type="datetime-local" name="reservationTime" onChange={handleChange} />
+        <h4>Additional Comments:</h4> <textarea type="text" name="comments" onChange={handleChange} />
         <button type="submit" className="submit">Submit Reservation</button>
       </form>
     </div>
