877ea3e3df65612dfe3292f751af55c1f95170a7
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2022-11-21 21:07:46 -0300

    Home buttons added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import './navbar.css';
+// import { Link } from 'react-router-dom'
 
 function Navbar() {
 
   return (
-    <nav className="navbar">
+    <nav>
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/Menu">Menu</a></li>
-        <li><a href="/book">Book</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/menu">Menu</a>
+        <a href="/book">Book</a>
+        <a href="/contact">Contact</a>
+
+        {/* <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/menu">Menu</Link>
+        <Link to="/book">Book</Link>
+        <Link to="/contact">Contact</Link> */}
+
       </ul>
     </nav>
   );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -3,37 +3,17 @@ import './footer.css';
 
 function Footer() {
 
-  return (
-
-		<footer className="footer-distributed">
-			<div className="footer-right">
+	return (
+		<footer>
+			<ul>
 				<a href="#"><i className="fa fa-facebook"></i></a>
+				<a href="#"><i className="fa fa-instagram"></i></a>
+				<a href="#"><i className="fa fa-whatsapp"></i></a>
 				<a href="#"><i className="fa fa-twitter"></i></a>
-				<a href="#"><i className="fa fa-linkedin"></i></a>
-				<a href="#"><i className="fa fa-github"></i></a>
-			</div>
-
-			<div className="footer-left">
-
-				<p className="footer-links">
-					<a className="link-1" href="#">Restaurant</a>
-
-					<a href="#">Home</a>
-
-					<a href="#">About</a>
-
-					<a href="#">Menu</a>
-
-					<a href="#">Book</a>
-
-					<a href="#">Contact</a>
-				</p>
-
-				<p>Retro Restaurant &copy; 2022</p>
-			</div>
-
+			</ul>
+			<p>Retro Restaurant &copy; 2022</p>
 		</footer>
-  );
+	);
 }
 
 export default Footer;
