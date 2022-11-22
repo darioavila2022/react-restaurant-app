78b674703f72d3d33addc0e1263c2c1fee42eb97
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2022-11-22 00:55:00 -0300

    Use of useNavigate

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 import './home.css'
 
-function Home () {
+function Home() {
+
+    let navigate = useNavigate();
+
     return (
         <div className='home-container'>
             <div>
-            <h1 className='home-title'>Retro Restaurant</h1>
-            <p>Where every flavor tells a story. A taste you will remember!</p>
-            <button className='home-button'>Menu</button>   
-            <button className='home-button'>Book</button>   
+                <h1 className='home-title'>Retro Restaurant</h1>
+                <p>Where every flavor tells a story. A taste you will remember!</p>
+                <button className='home-button' onClick={() => { navigate("/menu"); }}>Menu</button>
+                <button className='home-button' onClick={() => { navigate("/book"); }}>Book</button>
             </div>
             <img src={require('../../images/restaurant-retro.jpg')} className='home-img'></img>
             <div>
-
-                </div>         
+            </div>
         </div>
     )
 }
